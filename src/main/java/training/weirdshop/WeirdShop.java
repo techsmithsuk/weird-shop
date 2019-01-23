8b9137f882d469427b76e34a0479fcef450414df
James Griffiths <james.griffiths@softwire.com>
2019-01-23 11:04:15 +0000

    "item.quality - item.quality" can be replaced by "0" (IntelliJ automatic refactoring)

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -49,7 +49,7 @@ class WeirdShop {
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
