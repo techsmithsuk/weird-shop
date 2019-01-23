b0d8c4903c3e6976e473a4a1aa28a0bc899c018b
James Griffiths <james.griffiths@softwire.com>
2019-01-23 11:09:02 +0000

    Split the code into two methods: "update item sell-in" and "update item quality"

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -9,52 +9,59 @@ class WeirdShop {
 
     void updateQuality() {
         for (Item item : items) {
-            if (!isGoldCoin(item)) {
-                item.sellIn = item.sellIn - 1;
-            }
+            updateItemSellIn(item);
+            updateItemQuality(item);
+        }
+    }
 
-            if (!isAgedBrie(item)
-                    && !isBackstagePass(item)) {
-                if (item.quality > 0) {
-                    if (!isGoldCoin(item)) {
-                        item.quality = item.quality - 1;
+    private void updateItemSellIn(Item item) {
+        if (!isGoldCoin(item)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void updateItemQuality(Item item) {
+        if (!isAgedBrie(item)
+                && !isBackstagePass(item)) {
+            if (item.quality > 0) {
+                if (!isGoldCoin(item)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (isBackstagePass(item)) {
+                    if (item.sellIn <= 10) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
+
+                    if (item.sellIn <= 5) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (item.sellIn < 0) {
+            if (!isAgedBrie(item)) {
+                if (!isBackstagePass(item)) {
+                    if (item.quality > 0) {
+                        if (!isGoldCoin(item)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (isBackstagePass(item)) {
-                        if (item.sellIn <= 10) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn <= 5) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.sellIn < 0) {
-                if (!isAgedBrie(item)) {
-                    if (!isBackstagePass(item)) {
-                        if (item.quality > 0) {
-                            if (!isGoldCoin(item)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
                 }
             }
         }
