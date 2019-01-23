c6041d5d0f0312c126b27b541e614599863e2a17
James Griffiths <james.griffiths@softwire.com>
2019-01-23 11:20:38 +0000

    Simplify checks for Gold Coin
    Now we have a nice "if-elseif--elseif-else" pattern where we do different rules for each type of item

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -35,11 +35,10 @@ class WeirdShop {
                 item.quality = item.quality + 1;
             }
 
-        }
-        else {
-            if (!isGoldCoin(item)) {
-                item.quality = item.quality - 1;
-            }
+        } else if (isGoldCoin(item)) {
+            // Gold Coins don't change the "quality"
+        } else {
+            item.quality = item.quality - 1;
         }
 
         if (item.sellIn < 0) {
@@ -47,10 +46,10 @@ class WeirdShop {
                 item.quality = item.quality + 1;
             } else if (isBackstagePass(item)) {
                 item.quality = 0;
+            } else if (isGoldCoin(item)) {
+                // Gold Coins don't change the "quality"
             } else {
-                if (!isGoldCoin(item)) {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         }
 
